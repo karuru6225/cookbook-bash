0e436d827beb2c62c18e908d9ca1249ab386e466
karuru6225 <e2hjhi@gmail.com>
2015-06-05 12:29:35 +0900

    bashの設定をskelではなく、profile.dに置くようにした。

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,36 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-file "/etc/skel/.bash_history" do
-	owner "root"
-	group "root"
-	mode "0644"
-end
+#file "/etc/skel/.bash_history" do
+#	owner "root"
+#	group "root"
+#	mode "0644"
+#end
+#
+#cookbook_file "/etc/skel/.bash_logout" do
+#	owner 'root'
+#	group 'root'
+#	mode '0644'
+#	source 'bash_logout'
+#end
+#
+#cookbook_file "/etc/skel/.bash_profile" do
+##	owner 'root'
+#	group 'root'
+#	mode '0644'
+#	source 'bash_profile'
+#end
 
-cookbook_file "/etc/skel/.bash_logout" do
+#cookbook_file "/etc/skel/.bash_colors" do
+#	owner 'root'
+#	group 'root'
+#	mode '0644'
+#	source 'bash_colors'
+#end
+
+#cookbook_file "/etc/skel/.bashrc" do
+#	owner 'root'
+#	group 'root'
+#	mode '0644'
+#	source 'bashrc'
+#end
+
+cookbook_file "/etc/profile.d/bash.sh" do
 	owner 'root'
 	group 'root'
 	mode '0644'
-	source 'bash_logout'
-end
-
-cookbook_file "/etc/skel/.bash_profile" do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source 'bash_profile'
-end
-
-cookbook_file "/etc/skel/.bash_colors" do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source 'bash_colors'
-end
-
-cookbook_file "/etc/skel/.bashrc" do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source 'bashrc'
+	source 'bash.sh'
 end
